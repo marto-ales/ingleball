892c6d7eb49d3416461cd90bf5ea64b265547d43
Martin Ales <martin@gcoop.coop>
2026-09-15 23:43:55 -0300

    Add trusted_proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: [
+            '10.0.0.0/8',
+            '172.16.0.0/12',
+            '192.168.0.0/16',
+        ]);
+
         $middleware->alias([
             'organizer' => \App\Http\Middleware\EnsureUserIsOrganizer::class,
         ]);
